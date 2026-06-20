00bea1e3cc0a43528aa128c2650b1bf4d9a0c269
Антон Кузянин <marswaitsfor@gmail.com>
2026-06-20 14:34:20 +0300

    Initial commit: Spring Boot application with Hello World endpoint

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.12"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "io.hexlet"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
